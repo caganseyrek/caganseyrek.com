fe027235bc1f244faef89a5948631456dfbd9fe4
Çağan Seyrek <caganseyrek@outlook.com.tr>
2025-02-02 22:30:34 +0300

    Updated project preview component

--- components/ProjectsList.tsx
+++ components/ProjectsList.tsx
@@ -24,28 +24,29 @@ const ProjectsList = ({ highlightedOnly = false }: ProjectPreviewProps) => {
           article: project.links.article_page_path !== null,
           package: project.links.package_page !== null,
         };
+        // TODO remake this
         return (
           <div
             key={project.slug}
-            className="w-[calc((100%-15px)/2)] flex flex-col items-start justify-start gap-[3px] max-[570px]:w-full">
-            <div className="bg-accent border p-md w-full rounded-t-lg flex flex-col gap-sm flex-1">
+            className="w-[calc((100%-15px)/2)] flex flex-col items-start justify-start max-[570px]:w-full">
+            <div className="bg-accent border border-b-0 p-md w-full rounded-t-lg flex flex-col gap-sm flex-1">
               <h3 className="text-xl tracking-tight font-semibold">{project.title}</h3>
               <div className="text-muted-foreground flex-1">{project.preview_text}</div>
               <TagListElement tags={String(project.tags).split(",")} />
             </div>
-            <div className="flex flex-row items-center justify-start gap-[3px] w-full">
+            <div className="flex flex-row items-center justify-start w-full">
               <LinkedButton
                 href={project.links.repository}
                 isExternal
-                className={`w-full rounded-none ${!has.article && !has.package ? "rounded-b-lg" : "rounded-bl-lg"}`}
-                linkClassName={!has.article && !has.package ? "w-full" : "w-[calc((100%-3px)/2)]"}>
+                className={`w-full rounded-none ${!has.article && !has.package ? "rounded-b-lg" : "rounded-bl-lg border-r-0"}`}
+                linkClassName={!has.article && !has.package ? "w-full" : "w-[calc(100%/2)]"}>
                 <SiGithub /> Source
               </LinkedButton>
               {project.links.article_page_path && (
                 <LinkedButton
                   href={project.links.article_page_path}
                   className="w-full rounded-none rounded-br-lg"
-                  linkClassName="w-[calc((100%-3px)/2)]">
+                  linkClassName="w-[calc(100%/2)]">
                   <ScrollText /> Article
                 </LinkedButton>
               )}
@@ -54,7 +55,7 @@ const ProjectsList = ({ highlightedOnly = false }: ProjectPreviewProps) => {
                   href={project.links.package_page}
                   isExternal
                   className="w-full rounded-none rounded-br-lg"
-                  linkClassName="w-[calc((100%-3px)/2)]">
+                  linkClassName="w-[calc(100%/2)]">
                   <Package /> Package
                 </LinkedButton>
               )}
