797f3d6fb199b7bc985fd621db75b97d49ee6aed
Çağan Seyrek <caganseyrek@outlook.com.tr>
2025-04-03 21:26:03 +0300

    Updated tailwind and moved the configs to the globals.css file

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
-module.exports = {
+/** @type {import('postcss-load-config').Config} */
+const config = {
   plugins: {
-    "postcss-import": {},
-    tailwindcss: {},
-    autoprefixer: {},
+    "@tailwindcss/postcss": {},
   },
 };
+
+export default config;
